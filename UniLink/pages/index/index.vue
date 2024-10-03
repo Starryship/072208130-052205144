9399f96cbfb439e8a5e3e0e4e88079e2f4c5129d
Starryship <321705682@qq.com>
2024-10-03 18:18:01 +0800

    Signed-off-by: Starryship <321705682@qq.com>

--- UniLink/pages/index/index.vue
+++ UniLink/pages/index/index.vue
@@ -1,5 +1,5 @@
 <template>
-	<view>
+	<view class="content">
 		<image class="logo" src="/static/logo.png"></image>
 		<view class="text-area">
 			<text class="title">{{title}}</text>
@@ -24,15 +24,29 @@
 </script>
 
 <style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
 	.logo {
-		height: 100px;
-		width: 100px;
-		margin: 100px auto 25px auto;
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
 	}
 
 	.title {
-		font-size: 18px;
+		font-size: 36rpx;
 		color: #8f8f94;
-    text-align: center;
 	}
 </style>
